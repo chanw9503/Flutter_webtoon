b16588cf23fe0866da100bd68f15bf1da788da6f
chanwoo <chanw9503@gmail.com>
2024-05-30 17:07:47 +0900

    Feat. 동작 감지 위젯 추가

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -15,40 +15,45 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 250,
-          //본래에는 자식이 부모가 자신의 영역을 침범하는걸 막는데, 이걸적용하면 부모가 자식의
-          //영역에 침범할 수 있다. 그래서 border가 먹는다.
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 15,
-                offset: const Offset(10, 10),
-                color: Colors.black.withOpacity(0.5),
-              )
-            ],
+    return GestureDetector(
+      onTap: () {
+        print('take me homeeee');
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 250,
+            //본래에는 자식이 부모가 자신의 영역을 침범하는걸 막는데, 이걸적용하면 부모가 자식의
+            //영역에 침범할 수 있다. 그래서 border가 먹는다.
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 15,
+                  offset: const Offset(10, 10),
+                  color: Colors.black.withOpacity(0.5),
+                )
+              ],
+            ),
+            child: Image.network(
+              thumb,
+              headers: const {
+                "User-Agent": headerAgent,
+              },
+            ),
           ),
-          child: Image.network(
-            thumb,
-            headers: const {
-              "User-Agent": headerAgent,
-            },
+          const SizedBox(
+            height: 10,
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 22,
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
